b6e1eb825d0dda9923f72f918cad18d07b4548a6
hex-cat-man <hex-cat-man@tutanota.com>
2023-12-03 01:23:48 +0100

    Day 1: Trebuchet?! --- Part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -3,6 +3,19 @@ use std::{
     io::{self, BufRead},
 };
 
+/// Damn elfs.
+const ELVISH: [(&str, char); 9] = [
+    ("one", '1'),
+    ("two", '2'),
+    ("three", '3'),
+    ("four", '4'),
+    ("five", '5'),
+    ("six", '6'),
+    ("seven", '7'),
+    ("eight", '8'),
+    ("nine", '9'),
+];
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     let mut sum = 0;
 
@@ -21,7 +34,20 @@ fn parse_calibration_value(s: impl AsRef<str>) -> io::Result<usize> {
     let mut first = None;
     let mut last = None;
 
-    for c in s.as_ref().chars() {
+    let s = s.as_ref();
+
+    for (i, mut c) in s.chars().enumerate() {
+        if c.is_ascii_alphabetic() {
+            let elvish = ELVISH
+                .iter()
+                .find(|(num_str, _)| s[..=i].ends_with(*num_str))
+                .map(|(_, cr)| cr);
+
+            if let Some(replace_char) = elvish {
+                c = *replace_char;
+            }
+        }
+
         if c.is_ascii_digit() {
             if first.is_none() {
                 first = Some(c);
@@ -52,6 +78,23 @@ mod tests {
             ("pqr3stu8vwx", 38),
             ("a1b2c3d4e5f", 15),
             ("treb7uchet", 77),
+        ] {
+            assert_eq!(parse_calibration_value(s).unwrap(), n)
+        }
+    }
+
+    #[test]
+    fn test_parse_calibration_value_part_two() {
+        for (s, n) in [
+            ("two1nine", 29),
+            ("eightwothree", 83),
+            ("abcone2threexyz", 13),
+            ("xtwone3four", 24),
+            ("4nineeightseven2", 42),
+            ("zoneight234", 14),
+            ("7pqrstsixteen", 76),
+            // Ahhhhhhhhhh!
+            ("2fiveshtds4oneightsjg", 28),
         ] {
             assert_eq!(parse_calibration_value(s).unwrap(), n)
         }
