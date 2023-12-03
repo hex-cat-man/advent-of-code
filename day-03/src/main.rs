d4f0dbe7b30515db12c5f7d5236c3548d96c7894
hex-cat-man <hex-cat-man@tutanota.com>
2023-12-03 19:37:45 +0100

    Day 3: Gear Ratios --- Part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -12,13 +12,15 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         lines.push(line?);
     }
 
-    println!("{}", Schematic(lines).sum());
+    let schematic = Schematic(lines);
+    println!("Part number sum: {}", schematic.part_num_sum());
+    println!("Gear ratio sum: {}", schematic.gear_ratio_sum());
 
     Ok(())
 }
 
 impl Schematic {
-    fn sum(&self) -> usize {
+    fn part_num_sum(&self) -> usize {
         let mut sum = 0;
         for (line_nr, line) in self.0.iter().enumerate() {
             let mut chars = line.chars().enumerate().peekable();
@@ -52,6 +54,63 @@ impl Schematic {
         sum
     }
 
+    fn gear_ratio_sum(&self) -> usize {
+        let mut sum = 0;
+        for (line_nr, line) in self.0.iter().enumerate() {
+            let mut chars = line.chars().enumerate().filter(|(_, c)| *c == '*');
+
+            // Check each "gear" position for EXACTLY two numbers.
+            while let Some((i, '*')) = chars.next() {
+                let mut nums = vec![];
+
+                if line_nr > 0 {
+                    if let Some(line) = self.0.get(line_nr - 1) {
+                        let (n1, n2) = self.get_number_at(line, i);
+
+                        if let Some(n) = n1 {
+                            nums.push(n);
+                        }
+
+                        if let Some(n) = n2 {
+                            nums.push(n);
+                        }
+                    }
+                }
+
+                // Look before and after the "gear" in the same line.
+                if let Some(line) = self.0.get(line_nr) {
+                    let (n1, n2) = self.get_number_at(line, i);
+
+                    if let Some(n) = n1 {
+                        nums.push(n);
+                    }
+
+                    if let Some(n) = n2 {
+                        nums.push(n);
+                    }
+                }
+
+                if let Some(line) = self.0.get(line_nr + 1) {
+                    let (n1, n2) = self.get_number_at(line, i);
+
+                    if let Some(n) = n1 {
+                        nums.push(n);
+                    }
+
+                    if let Some(n) = n2 {
+                        nums.push(n);
+                    }
+                }
+
+                if nums.len() == 2 {
+                    sum += nums[0] * nums[1];
+                }
+            }
+        }
+
+        sum
+    }
+
     fn range_contains_part_symbol(&self, line: &str, range: ops::Range<usize>) -> bool {
         for pos in range {
             if let Some(c) = line.chars().nth(pos) {
@@ -62,6 +121,67 @@ impl Schematic {
         }
 
         false
+    }
+
+    /// Get up to two numbers at the given "gear" position.
+    fn get_number_at(&self, line: &str, pos: usize) -> (Option<usize>, Option<usize>) {
+        let mut chars = line.chars().peekable();
+
+        if chars.nth(pos).is_some_and(|c| c.is_ascii_digit()) {
+            (
+                // Only find the start of the number to the left and follow it to the end.
+                {
+                    let mut start = pos;
+                    while start > 0
+                        && line
+                            .chars()
+                            .nth(start - 1)
+                            .is_some_and(|c| c.is_ascii_digit())
+                    {
+                        start -= 1;
+                    }
+
+                    line.chars()
+                        .skip(start)
+                        .take_while(|c| c.is_ascii_digit())
+                        .collect::<String>()
+                        .parse::<usize>()
+                        .ok()
+                },
+                None,
+            )
+        } else {
+            (
+                // Check to the left of pos. If it is a digit, find the start of the number.
+                {
+                    let mut start = pos;
+                    while start > 0
+                        && line
+                            .chars()
+                            .nth(start - 1)
+                            .is_some_and(|c| c.is_ascii_digit())
+                    {
+                        start -= 1;
+                    }
+
+                    line.chars()
+                        .skip(start)
+                        .take_while(|c| c.is_ascii_digit())
+                        .collect::<String>()
+                        .parse::<usize>()
+                        .ok()
+                },
+                // Check to the right of pos. If it is a digit, parse it and all following digits.
+                {
+                    line.chars()
+                        .skip(pos + 1)
+                        .take_while(|c| c.is_ascii_digit())
+                        .collect::<String>()
+                        .parse::<usize>()
+                        .ok()
+                },
+            )
+        }
     }
 
     fn is_part(&self, line_nr: usize, start: usize, end: usize) -> bool {
@@ -114,6 +234,24 @@ mod tests {
             ".664.598..".into(),
         ]);
 
-        assert_eq!(schematic.sum(), 4361);
+        assert_eq!(schematic.part_num_sum(), 4361);
+    }
+
+    #[test]
+    fn example_part_two() {
+        let schematic = Schematic(vec![
+            "467..114..".into(),
+            "...*......".into(),
+            "..35..633.".into(),
+            "......#...".into(),
+            "617*......".into(),
+            ".....+.58.".into(),
+            "..592.....".into(),
+            "......755.".into(),
+            "...$.*....".into(),
+            ".664.598..".into(),
+        ]);
+
+        assert_eq!(schematic.gear_ratio_sum(), 467835);
     }
 }
