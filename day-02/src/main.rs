0a9ebe2dfb746d8c02cebf5ba872d4a15b76f2ac
hex-cat-man <hex-cat-man@tutanota.com>
2023-12-03 16:17:09 +0100

    Day 2: Cube Conundrum --- Part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -20,15 +20,19 @@ struct Game {
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let mut sum = 0;
+    let mut pow_sum = 0;
     for line in io::stdin().lock().lines() {
         let game: Game = line?.parse()?;
 
         if game.is_possible(12, 13, 14) {
             sum += game.id;
         }
+
+        pow_sum += game.required_pow();
     }
 
-    println!("{sum}");
+    println!("Sum of possible game ids: {sum}");
+    println!("Sum of minimal cube power: {pow_sum}");
 
     Ok(())
 }
@@ -41,6 +45,10 @@ impl Game {
             .max()
             .cloned()
             .unwrap_or_default()
+    }
+
+    fn required_pow(&self) -> usize {
+        self.max(&Cube::Red) * self.max(&Cube::Green) * self.max(&Cube::Blue)
     }
 
     fn is_possible(&self, red: usize, green: usize, blue: usize) -> bool {
@@ -192,12 +200,10 @@ mod tests {
 
     #[test]
     fn possible_game() {
-        assert!(
-            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-                .parse::<Game>()
-                .unwrap()
-                .is_possible(12, 13, 14)
-        );
+        assert!("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+            .parse::<Game>()
+            .unwrap()
+            .is_possible(12, 13, 14));
 
         assert!(
             "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
@@ -220,11 +226,25 @@ mod tests {
                 .is_possible(12, 13, 14)
         );
 
-        assert!(
-            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-                .parse::<Game>()
-                .unwrap()
-                .is_possible(12, 13, 14)
-        );
+        assert!("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+            .parse::<Game>()
+            .unwrap()
+            .is_possible(12, 13, 14));
+    }
+
+    #[test]
+    fn required_power() {
+        let mut sum = 0;
+        for game in [
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ] {
+            sum += game.parse::<Game>().unwrap().required_pow();
+        }
+
+        assert_eq!(sum, 2286)
     }
 }
