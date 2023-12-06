7ae6e8a934e40c568b093b73189e37d75b5526c8
hex-cat-man <hex-cat-man@tutanota.com>
2023-12-06 12:17:38 +0100

    Day 6: Wait For It --- Part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,17 +1,22 @@
-use std::io;
+use std::{env, io};
 
 fn main() {
-    let input = split_pairs(
-        io::stdin()
-            .lines()
-            .map_while(Result::ok)
-            .fold(String::new(), |s, l| s + &l + "\n"),
-    );
+    let part1_flag = env::args().skip(1).any(|flag| flag == "--part1");
 
-    println!("{}", solve_part_1(input));
+    let input = io::stdin()
+        .lines()
+        .map_while(Result::ok)
+        .fold(String::new(), |s, l| s + &l + "\n");
+    let input = if part1_flag {
+        split_pairs(input)
+    } else {
+        vec![parse_pair(input)]
+    };
+
+    println!("{}", solve(input));
 }
 
-fn solve_part_1(pairs: Vec<(usize, usize)>) -> usize {
+fn solve(pairs: Vec<(usize, usize)>) -> usize {
     let mut product = 1;
 
     for (time, distance) in pairs {
@@ -32,6 +37,30 @@ fn solve_part_1(pairs: Vec<(usize, usize)>) -> usize {
     product
 }
 
+fn parse_pair(input: impl AsRef<str>) -> (usize, usize) {
+    let mut lines = input.as_ref().lines();
+
+    let time = lines
+        .next()
+        .expect("missing time line")
+        .split_whitespace()
+        .skip(1)
+        .collect::<String>()
+        .parse::<usize>()
+        .expect("invalid time");
+
+    let distance = lines
+        .next()
+        .expect("missing distance line")
+        .split_whitespace()
+        .skip(1)
+        .collect::<String>()
+        .parse::<usize>()
+        .expect("invalid distance");
+
+    (time, distance)
+}
+
 fn split_pairs(input: impl AsRef<str>) -> Vec<(usize, usize)> {
     let mut lines = input.as_ref().lines();
 
@@ -42,7 +71,7 @@ fn split_pairs(input: impl AsRef<str>) -> Vec<(usize, usize)> {
         .skip(1)
         .map(|s| s.parse::<usize>().expect("invalid distance"));
 
-    let mut distance = lines
+    let mut distances = lines
         .next()
         .expect("missing distance line")
         .split_whitespace()
@@ -50,7 +79,7 @@ fn split_pairs(input: impl AsRef<str>) -> Vec<(usize, usize)> {
         .map(|s| s.parse::<usize>().expect("invalid distance"));
 
     times
-        .map(|time| (time, distance.next().expect("missing distance")))
+        .map(|time| (time, distances.next().expect("missing distance")))
         .collect()
 }
 
@@ -65,6 +94,16 @@ mod tests {
         let pairs = split_pairs(include_str!("../sample.txt"));
 
         assert_eq!(pairs, vec![(7, 9), (15, 40), (30, 200)]);
-        assert_eq!(solve_part_1(pairs), 288);
+        assert_eq!(solve(pairs), 288);
+    }
+
+    #[test]
+    fn example_part_2() {
+        // Time:      7  15   30
+        // Distance:  9  40  200
+        let pair = parse_pair(include_str!("../sample.txt"));
+
+        assert_eq!(pair, (71530, 940200));
+        assert_eq!(solve(vec![pair]), 71503);
     }
 }
