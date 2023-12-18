fdc6b7e0d43b047ce49973fea2f0deae25d3224d
hex-cat-man <hex-cat-man@tutanota.com>
2023-12-18 21:14:15 +0100

    Day 18: Lavaduct Lagoon --- Part 2

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ stop after the first day (like every Year)?
 15. [x] [Day 15: Lens Library](https://adventofcode.com/2023/day/15)
 16. [x] [Day 16: The Floor Will Be Lava](https://adventofcode.com/2023/day/16)
 17. -
-18. [ ] [Day 18: Lavaduct Lagoon](https://adventofcode.com/2023/day/18)
+18. [x] [Day 18: Lavaduct Lagoon](https://adventofcode.com/2023/day/18)
 
 ## Running the challenges
 
--- src/bin/day_18.rs
+++ src/bin/day_18.rs
@@ -9,28 +9,35 @@ use std::{error, io};
 
 type Error = Box<dyn error::Error>;
 
-type Pos = (isize, isize);
+type Pos = (i64, i64);
 
 fn main() -> Result<(), Error> {
-    let (coordinates, boundary) = coordinates_from_lines(io::stdin().lines().map_while(Result::ok))?;
-    let result = area(coordinates, boundary as isize);
+    #[cfg(feature = "part1")]
+    let (coordinates, boundary) =
+        coordinates_from_lines_part_1(io::stdin().lines().map_while(Result::ok))?;
+    #[cfg(not(feature = "part1"))]
+    let (coordinates, boundary) =
+        coordinates_from_lines(io::stdin().lines().map_while(Result::ok))?;
+
+    let result = area(coordinates, boundary);
     println!("{result}");
 
     Ok(())
 }
 
-fn coordinates_from_lines(
+#[cfg(any(test, feature = "part1"))]
+fn coordinates_from_lines_part_1(
     lines: impl Iterator<Item = impl AsRef<str>>,
-) -> Result<(Vec<Pos>, usize), Error> {
+) -> Result<(Vec<Pos>, i64), Error> {
     let mut coordinates = Vec::new();
-    let mut pos: (isize, isize) = (0, 0);
+    let mut pos: (i64, i64) = (0, 0);
     let mut boundary = 0;
     for line in lines {
         coordinates.push(pos);
         let mut split = line.as_ref().split_whitespace();
         let direction = split.next().ok_or("no direction")?;
-        let len = split.next().ok_or("no len")?.parse::<isize>()?;
-        boundary += len as usize;
+        let len = split.next().ok_or("no len")?.parse::<i64>()?;
+        boundary += len;
 
         let (x, y) = pos;
         pos = match direction {
@@ -45,14 +52,49 @@ fn coordinates_from_lines(
     Ok((coordinates, boundary))
 }
 
-fn area(mut coordinates: Vec<Pos>, boundary: isize) -> isize {
-    eprintln!("{coordinates:?}");
+#[cfg(any(test, not(feature = "part1")))]
+fn coordinates_from_lines(
+    lines: impl Iterator<Item = impl AsRef<str>>,
+) -> Result<(Vec<Pos>, i64), Error> {
+    let mut coordinates = Vec::new();
+    let mut pos: (i64, i64) = (0, 0);
+    let mut boundary = 0;
+    for line in lines {
+        coordinates.push(pos);
+        let hex = line
+            .as_ref()
+            .split_whitespace()
+            .nth(2)
+            .ok_or("missing instruction")?;
 
+        if hex.len() != 9 {
+            return Err("instruction to short".into());
+        }
+
+        let direction = hex.chars().nth(7).ok_or("missing direction")?;
+        let len = i64::from_str_radix(&hex[2..7], 16)?;
+
+        boundary += len;
+
+        let (x, y) = pos;
+        pos = match direction {
+            '3' => (x, y - len),
+            '0' => (x + len, y),
+            '1' => (x, y + len),
+            '2' => (x - len, y),
+            invalid => return Err(format!("'{invalid}': invalid direction").into()),
+        };
+    }
+
+    Ok((coordinates, boundary))
+}
+
+fn area(mut coordinates: Vec<Pos>, boundary: i64) -> i64 {
     if let Some(pos) = coordinates.first().cloned() {
         coordinates.push(pos);
     }
 
-    let mut sum: isize = 0;
+    let mut sum: i64 = 0;
     for (i, pos_b) in coordinates.iter().enumerate().skip(1) {
         let pos_a = coordinates.get(i - 1).expect("I did an of by one");
         sum += (pos_a.0 * pos_b.1) - (pos_a.1 * pos_b.0);
@@ -77,9 +119,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example() {
+    fn example_part_1() {
+        let (coordinates, boundary) =
+            coordinates_from_lines_part_1(include_str!("../../examples/18.txt").lines()).unwrap();
+        assert_eq!(62, area(coordinates, boundary));
+    }
+
+    #[test]
+    fn example_part_2() {
         let (coordinates, boundary) =
             coordinates_from_lines(include_str!("../../examples/18.txt").lines()).unwrap();
-        assert_eq!(62, area(coordinates, boundary as isize));
+        assert_eq!(952408144115, area(coordinates, boundary));
     }
 }
